448c9c937fd08f6515c83e17a5d4192bd1c04400
Humayoun Syed <139584890+Humayounsyed2005@users.noreply.github.com>
2024-02-27 20:30:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function linked(){
+    alert("Sorry Page is not Availible!");
+}
+function login(){
+    alert("Enter Your Email First!");
+}
+
+
+
+function toggleAnswer(element) {
+    const answer = element.querySelector('.answer');
+    answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+}
